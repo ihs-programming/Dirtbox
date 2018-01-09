f149075744dd5339fba0e33ba6571430732f90c3
s-ballw <s-ballw@54S50012556.it.bsd405.org>
2018-01-09 09:09:00 -0800

    Made world height a variable separate from world size, ore spawns more

--- src/game/World.java
+++ src/game/World.java
@@ -11,15 +11,17 @@ import game.entities.Entity;
 
 public class World {
 	private final static int DEBUG_WORLD_DEFAULT_SIZE = 100;
+	private final static int DEBUG_WORLD_DEFAULT_HEIGHT = 100;
 
-	Block[][] blocks = new Block[DEBUG_WORLD_DEFAULT_SIZE][DEBUG_WORLD_DEFAULT_SIZE];
+	Block[][] blocks = new Block[DEBUG_WORLD_DEFAULT_SIZE][DEBUG_WORLD_DEFAULT_HEIGHT];
 	ArrayList<Entity> characters;
 
 	public World() {
 		generateWorld();
 		characters = new ArrayList<>();
 		try {
-			Entity stalin = new Entity(new Image("data/characters/stalin.jpg"), 1, 1, new Vector2f(0, 0));
+			Entity stalin = new Entity(new Image("data/characters/stalin.jpg"), 1, 1,
+					new Vector2f(0, 0));
 			characters.add(stalin);
 			stalin.magnify(.1f);
 		} catch (SlickException e) {
@@ -29,9 +31,9 @@ public class World {
 	}
 
 	public void draw(Viewport vp) {
-		for (int i = 0; i < blocks.length; i++) {
-			for (int j = 0; j < blocks[i].length; j++) {
-				blocks[i][j].draw(vp);
+		for (Block[] block : blocks) {
+			for (Block element : block) {
+				element.draw(vp);
 			}
 		}
 		for (Entity e : this.characters) {
@@ -41,7 +43,7 @@ public class World {
 
 	/**
 	 * TODO Loads a file specifying the world map into the game
-	 * 
+	 *
 	 * @param filename
 	 * @throws IOException
 	 */
@@ -53,45 +55,50 @@ public class World {
 	 */
 	public void generateWorld() {
 		int blocksez[][];
-		blocksez = new int[DEBUG_WORLD_DEFAULT_SIZE][DEBUG_WORLD_DEFAULT_SIZE];
+		blocksez = new int[DEBUG_WORLD_DEFAULT_SIZE][DEBUG_WORLD_DEFAULT_HEIGHT];
 		for (int i = 0; i < DEBUG_WORLD_DEFAULT_SIZE; i++) {
-			int depth = DEBUG_WORLD_DEFAULT_SIZE - 1;
+			int depth = DEBUG_WORLD_DEFAULT_HEIGHT - 1;
 			blocks[i][depth] = new SolidBlock(BlockType.STONE, i * Block.BLOCK_SPRITE_SIZE,
 					depth * Block.BLOCK_SPRITE_SIZE);
 			blocksez[i][depth] = 3;
 		}
-		for (int j = 98; (j >= 0); j--) {
+		for (int j = DEBUG_WORLD_DEFAULT_HEIGHT - 2; j >= 0; j--) {
 			for (int i = 0; i < DEBUG_WORLD_DEFAULT_SIZE; i++) {
-				int empty = (int) (Math.random() + (j / 30.0));
+				int empty = (int) (Math.random() + j / (DEBUG_WORLD_DEFAULT_HEIGHT * 30.0 / 100.0));
 				int blockType = 0;
 
 				if (blocksez[i][j + 1] != 0) {
 					blocksez[i][j] = empty;
 					blockType = empty;
-					if (empty==1&&!(i-1<0)&&!(i+1>=DEBUG_WORLD_DEFAULT_SIZE)) {
-						if ((blocksez[i+1][j+1]!=0)&&(blocksez[i-1][j+1]!=0)) {
+					if (empty - 2 >= Math.random() * 100) {
+						blockType = 4;
+						blocksez[i][j] = 4;
+					}
+					if (empty == 1 && !(i - 1 < 0) && !(i + 1 >= DEBUG_WORLD_DEFAULT_SIZE)) {
+						if (blocksez[i + 1][j + 1] != 0 && blocksez[i - 1][j + 1] != 0) {
 							blocksez[i][j] = empty;
 							blockType = empty;
-						}else {
+
+						} else {
 							blocksez[i][j] = 0;
 							blockType = 0;
 						}
 					}
 				}
-				
+
 				if (blocksez[i][j + 1] == 5) {
 					blocksez[i][j] = 0;
 					blockType = 0;
 				}
-				if (blocksez[i][j + 1] == (1) && blocksez[i][j + 2] != (5) && blocksez[i][j + 1] != (0) && j < 30.0 * Math.random()) {
+				if (blocksez[i][j + 1] == 1 && blocksez[i][j + 2] != 5 && blocksez[i][j + 1] != 0
+						&& j < 30.0 * Math.random()) {
 					blocksez[i][j] = 5;
 					blockType = 5;
 				}
-				if (blocksez[i][j+1]==0){
+				if (blocksez[i][j + 1] == 0) {
 					blocksez[i][j] = 0;
 					blockType = 0;
 				}
-				
 
 				BlockType type = BlockType.EMPTY;
 				switch (blockType) {
@@ -116,7 +123,8 @@ public class World {
 				default:
 					type = BlockType.UNDEFINED;
 				}
-				blocks[i][j] = new SolidBlock(type, i * Block.BLOCK_SPRITE_SIZE, j * Block.BLOCK_SPRITE_SIZE);
+				blocks[i][j] = new SolidBlock(type, i * Block.BLOCK_SPRITE_SIZE,
+						j * Block.BLOCK_SPRITE_SIZE);
 			}
 		}
 	}
