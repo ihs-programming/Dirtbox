733dd491d2dd848cf0e50a7723a50754acbedcdd
s-weia <s-weia@54L1809-16.it.bsd405.org>
2018-01-18 16:29:39 -0800

    Rewrote world generation

--- src/game/RegionGenerator.java
+++ src/game/RegionGenerator.java
@@ -8,6 +8,7 @@ import org.newdawn.slick.geom.Rectangle;
 import game.blocks.Block;
 import game.blocks.BlockType;
 import game.blocks.SolidBlock;
+import util.ImprovedNoise;
 
 public class RegionGenerator {
 	private static final int CHUNK_HEIGHT = 127;
@@ -57,7 +58,93 @@ public class RegionGenerator {
 		}
 	}
 
+	private double count = 3000;
+
 	private Block[][] generateChunk(int x, int y) {
+		long chunkgenerationtime = System.nanoTime();
+		BiomeType biometype = randombiome(); // selects a random biome
+
+		Block[][] blocks = new Block[CHUNK_SIZE][CHUNK_HEIGHT];
+		int[] heightMap = new int[CHUNK_SIZE];
+		for (int i = 0; i < heightMap.length; i++) {
+			heightMap[i] = (int) (10 * ImprovedNoise.noise(count, 1, 1)
+					+ CHUNK_SIZE / 2);
+			count += 0.05;
+			System.out.println(count);
+		}
+		for (int i = 0; i < CHUNK_SIZE; i++) {
+			for (int z = 0; z < heightMap[i]; z++) {
+				blocks[i][z] = new SolidBlock(BlockType.EMPTY,
+						(i + x) * Block.BLOCK_SPRITE_SIZE,
+						(z + y) * Block.BLOCK_SPRITE_SIZE);
+			}
+			for (int z = heightMap[i]; z < CHUNK_HEIGHT; z++) {
+				BlockType type = getType(z - heightMap[i], biometype);
+				blocks[i][z] = new SolidBlock(type,
+						(i + x) * Block.BLOCK_SPRITE_SIZE,
+						(z + y) * Block.BLOCK_SPRITE_SIZE);
+			}
+		}
+
+		if (biometype == BiomeType.DESERT) {
+			int height = Math.max(heightMap[0], heightMap[heightMap.length - 1]) + 1;
+			for (int i = 0; i < heightMap.length; i++) {
+				for (int z = height; z < CHUNK_HEIGHT; z++) {
+					if (((SolidBlock) blocks[i][z]).type == BlockType.EMPTY) {
+						blocks[i][z] = new SolidBlock(BlockType.WATER,
+								(i + x) * Block.BLOCK_SPRITE_SIZE,
+								(z + y) * Block.BLOCK_SPRITE_SIZE);
+					}
+				}
+			}
+		}
+
+		if (Viewport.DEBUG_MODE) {
+			System.out.println((System.nanoTime() - chunkgenerationtime) / 1000000.0
+					+ " ms to generate chunk of type " + biometype);
+		}
+		return blocks;
+	}
+
+	/**
+	 * Get the block type.
+	 *
+	 * @param y
+	 *            Y is DISTANCE RELATIVE TO SURFACE.
+	 * @param biome
+	 * @return
+	 */
+	private BlockType getType(int y, BiomeType biome) {
+		if (y < 5 + 2 * Math.random()) {
+			switch (biome) {
+			case DESERT:
+				return BlockType.SAND;
+			case MOUNTAIN:
+				return BlockType.STONE;
+			}
+
+			if (y == 0) {
+				return BlockType.GRASS;
+			}
+			return BlockType.DIRT;
+		}
+		if (Math.random() < 0.01) {
+			double val = Math.random();
+			if (val < 0.1) {
+				return BlockType.DIAMOND_ORE;
+			} else if (val < 0.2) {
+				return BlockType.REDSTONE_ORE;
+			} else if (val < 0.3) {
+				return BlockType.GOLD_ORE;
+			} else {
+				return BlockType.COAL_ORE;
+			}
+		}
+
+		return BlockType.STONE;
+	}
+
+	private Block[][] generateChunk2(int x, int y) {
 		long chunkgenerationtime = System.nanoTime();
 		BiomeType biometype = randombiome(); // selects a random biome
 
