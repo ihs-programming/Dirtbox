407954ab397c149ad436601fa8fb8c04d3a7f411
gamester543 <gamesterrex@gmail.com>
2018-02-03 18:05:06 -0800

    Some blocks fall.

--- src/game/World.java
+++ src/game/World.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NavigableSet;
+import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.TreeMap;
@@ -50,7 +51,7 @@ public class World {
 
 	private Input userInp = null; // used only for debugging purposes currently
 
-	public TreeMap<Point, Block> blocks = new TreeMap<>(pointComparer);
+	private TreeMap<Point, Block> blocks = new TreeMap<>(pointComparer);
 
 	public World() {
 		characters = new ArrayList<>();
@@ -348,7 +349,73 @@ public class World {
 
 			}
 		}
+	}
 
+	private Set<Point> changedBlocks = new HashSet<Point>();
+
+	private void propagateFallingBlocks(HashSet<Point> queue) {
+
+		Iterator<Point> iter = changedBlocks.iterator();
+		while (iter.hasNext()) {
+			Point p = iter.next();
+			Point above = new Point(p.x, p.y - 1);
+			if (blocks.containsKey(above) && blocks.containsKey(p)) {
+				if (blocks.get(p).type == BlockType.EMPTY) {
+					Block onTop = blocks.get(above);
+					if (onTop.type == BlockType.WATER || onTop.type == BlockType.SAND) {
+						swapBlocks(p, above);
+
+						addAround(queue, p);
+					}
+				}
+			}
+		}
+	}
+
+	private void propagateLiquids() {
+		HashSet<Point> queue = new HashSet<>();
+		propagateFallingBlocks(queue);
+
+		HashSet<Point> possibleLiquids = new HashSet<>();
+		for (Point p : changedBlocks) {
+			addAround(possibleLiquids, p);
+		}
+		for (Point p : possibleLiquids) {
+			Block b = blocks.get(p);
+			if (b instanceof LiquidBlock) {
+				// Hmm.. Might as well use it? This probably isn't how you're
+				// supposed to tho.
+				Optional<Block> under = Optional.of(blocks.get(new Point(p.x, p.y + 1)));
+				if (under.isPresent()) {
+					swapBlocks(p, new Point(p.x, p.y + 1));
+					addAround(queue, p);
+				}
+			}
+		}
+
+		changedBlocks = queue;
+
+	}
+
+	private void swapBlocks(Point a, Point b) {
+		if (blocks.containsKey(a) && blocks.containsKey(b)) {
+			Block first = blocks.get(a);
+			Block second = blocks.get(b);
+			if (first.type == BlockType.EMPTY || second.type == BlockType.EMPTY) {
+				Vector2f oriPos = first.getPos();
+				Vector2f newPos = second.getPos();
+				blocks.put(a, Block.createBlock(second.type, oriPos.x, oriPos.y));
+				blocks.put(b, Block.createBlock(first.type, newPos.x, newPos.y));
+			}
+		}
+	}
+
+	private void addAround(Set<Point> queue, Point p) {
+		for (int i = -1; i <= 1; i++) {
+			for (int z = -1; z <= 1; z++) {
+				queue.add(new Point(p.x + i, p.y + z));
+			}
+		}
 	}
 
 	public List<Point> getVisibleBlockLocations(Rectangle view) {
@@ -386,6 +453,8 @@ public class World {
 				iter.remove();
 			}
 		}
+
+		propagateLiquids();
 
 		// collision detection for main character
 		for (Entity e : characters) {
@@ -451,6 +520,8 @@ public class World {
 		Vector2f bpos = b.getPos();
 		Point blockLoc = new Point(round(bpos.x), round(bpos.y));
 		blocks.put(blockLoc, Block.createBlock(BlockType.EMPTY, bpos.x, bpos.y));
+
+		changedBlocks.add(blockLoc);
 	}
 
 	private int round(float f) {
