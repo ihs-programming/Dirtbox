ba4c7b5cd620610194cacd7bb9c88dbe3ad50803
s-chenrob <s-chenrob@54S50037293.it.bsd405.org>
2018-03-01 16:31:57 -0800

    Rewrote command system. TODO.

--- src/game/utils/Console.java
+++ src/game/utils/Console.java
@@ -1,10 +1,8 @@
 package game.utils;
 
 import java.awt.Dimension;
-import java.awt.Point;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.IllegalFormatException;
 import java.util.Map;
@@ -14,7 +12,6 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
-import game.Viewport;
 import game.entities.ControllableCharacter;
 import game.network.Client;
 import game.network.Server;
@@ -25,6 +22,16 @@ import game.world.World;
  * Handles various commands in the game
  */
 public class Console extends Thread {
+	static {
+		addCommand("ping", args -> "Pong");
+
+		// You can add commands from other places as well!
+	}
+
+	private static interface CommandParser {
+		public String command(String args[]);
+	}
+
 	private ControllableCharacter character;
 	private World world;
 	private Client client = new Client();
@@ -58,128 +65,88 @@ public class Console extends Thread {
 		frame.pack();
 	}
 
-	public String doCommand(String input) {
+	private static HashMap<String, CommandParser> commands = new HashMap<>();
 
+	public static void addCommand(String text, CommandParser cp) {
+		commands.put(text, cp);
+	}
+
+	public static final String ERROR = "Unknown command";
+
+	public String doCommand(String input) {
+		String[] args = input.split(" ");
+		if (commands.containsKey(args[0])) {
+			return commands.get(args[0]).command(args);
+		}
+		return ERROR;
 		// List of all commands and what they do
 		// note that commandhelp must be of the form [(command),(command)...] :
 		// (helptext)
-		ArrayList<String> commandhelp = new ArrayList<>();
-		commandhelp.add("!help, !h, !? : returns a list of commands");
-		commandhelp.add("!time : returns the time");
-		commandhelp.add("!time set [time] : sets the current time to [time]");
-		commandhelp.add("!fly : increases movement speed tenfold");
-		commandhelp.add("!listservers : lists availible servers");
-		commandhelp.add("!host : starts hosting server on computer");
-		commandhelp.add("!stophosting: stops the current server (if running)");
-		commandhelp.add(
-				"!explode : Breaks all blocks in a large radius around the player");
-		commandhelp.add(
-				"!connect [number] : connects to server denoted by number created from !listservers");
-		commandhelp.add("!disconnect : Disconnects from currently connected server");
-		commandhelp.add("!viewmessages : Views all messages from hosts");
-		commandhelp.add(
-				"!send [message] : sends a message to the currently connected server");
-		commandhelp.add(
-				"!constatus : prints information about the currently connected to server");
-		if (input.startsWith("!")) {
-			String command[] = input.split(" ");
-			return executeCommand(command, commandhelp);
-		}
-		return null;
-	}
+		/*
+		 * ArrayList<String> commandhelp = new ArrayList<>();
+		 * commandhelp.add("!help, !h, !? : returns a list of commands");
+		 * commandhelp.add("!time : returns the time");
+		 * commandhelp.add("!time set [time] : sets the current time to [time]");
+		 * commandhelp.add("!fly : increases movement speed tenfold");
+		 * commandhelp.add("!listservers : lists availible servers");
+		 * commandhelp.add("!host : starts hosting server on computer");
+		 * commandhelp.add("!stophosting: stops the current server (if running)");
+		 * commandhelp.add(
+		 * "!explode : Breaks all blocks in a large radius around the player");
+		 * commandhelp.add(
+		 * "!connect [number] : connects to server denoted by number created from !listservers"
+		 * );
+		 * commandhelp.add("!disconnect : Disconnects from currently connected server"
+		 * ); commandhelp.add("!viewmessages : Views all messages from hosts");
+		 * commandhelp.add(
+		 * "!send [message] : sends a message to the currently connected server");
+		 * commandhelp.add(
+		 * "!constatus : prints information about the currently connected to server");
+		 * if (input.startsWith("!")) { String command[] = input.split(" "); return
+		 * executeCommand(command, commandhelp); } return null; }
+		 *
+		 * public String executeCommand(String command[], ArrayList<String> commandhelp)
+		 * { String output = ""; // return help value if (command.length > 1) { if
+		 * (command[1].equals("?")) { for (int i = 0; i < commandhelp.size(); i++) { if
+		 * (commandhelp.get(i).startsWith(command[0])) { output += commandhelp.get(i) +
+		 * "\n"; } } return output; } }
+		 *
+		 * boolean commandExists = false; for (String help : commandhelp) { if
+		 * (help.split(":")[0].matches(".*\\b" + command[0].substring(1) + "\\b.*")) {
+		 * commandExists = true; break; } } if (!commandExists) { output += "\"" +
+		 * command[0] + "\" is not a recognized command. Use \"!help\" for help\n";
+		 * return output; }
+		 *
+		 * output += runGameCommand(command); output += runNetworkCommand(command);
+		 * output += getNetworkStatus(command);
+		 *
+		 * switch (command[0]) { case "!h": case "!help": case "!?": for (int i = 0; i <
+		 * commandhelp.size(); i++) { output += commandhelp.get(i) + "\n"; } break; }
+		 * return output; }
+		 *
+		 * private String runGameCommand(String[] command) { String output = "";
+		 *
+		 * switch (command[0]) { // "!time" command, sets and returns time case "!time":
+		 * if (command.length < 2) { output += "Time is: " + Viewport.globaltimer +
+		 * "\n"; } else { if (command.length > 1 && command[1].equals("set")) { try {
+		 * Viewport.globaltimer = Long.parseLong(command[2]); output += "Time set to " +
+		 * Viewport.globaltimer + "\n"; } catch (NumberFormatException e) { output +=
+		 * "\"" + command[2] + "\" is not a valid time. Use \"!time ?\" for help\n"; } }
+		 * } break;
+		 *
+		 * case "!save": saver.save(world, world.regionGenerator); output = null; break;
+		 *
+		 * case "!load": saver.load(world); output = null; break;
+		 *
+		 * // "!fly" command, changes flying state case "!f": case "!fly":
+		 * character.flying = !character.flying; output = null; break; case
+		 * "!addhealth": character.doHit(-10000); break;
+		 *
+		 * case "!explode": Point p = new Point((int) character.getHitbox().getX(),
+		 * (int) character.getHitbox().getY()); world.explode(p, 20); output = null;
+		 * break; } return output;
+		 */
 
-	public String executeCommand(String command[], ArrayList<String> commandhelp) {
-		String output = "";
-		// return help value
-		if (command.length > 1) {
-			if (command[1].equals("?")) {
-				for (int i = 0; i < commandhelp.size(); i++) {
-					if (commandhelp.get(i).startsWith(command[0])) {
-						output += commandhelp.get(i) + "\n";
-					}
-				}
-				return output;
-			}
-		}
-
-		boolean commandExists = false;
-		for (String help : commandhelp) {
-			if (help.split(":")[0].matches(".*\\b" + command[0].substring(1) + "\\b.*")) {
-				commandExists = true;
-				break;
-			}
-		}
-		if (!commandExists) {
-			output += "\"" + command[0]
-					+ "\" is not a recognized command. Use \"!help\" for help\n";
-			return output;
-		}
-
-		output += runGameCommand(command);
-		output += runNetworkCommand(command);
-		output += getNetworkStatus(command);
-
-		switch (command[0]) {
-		case "!h":
-		case "!help":
-		case "!?":
-			for (int i = 0; i < commandhelp.size(); i++) {
-				output += commandhelp.get(i) + "\n";
-			}
-			break;
-		}
-		return output;
-	}
-
-	private String runGameCommand(String[] command) {
-		String output = "";
-
-		switch (command[0]) {
-		// "!time" command, sets and returns time
-		case "!time":
-			if (command.length < 2) {
-				output += "Time is: " + Viewport.globaltimer + "\n";
-			} else {
-				if (command.length > 1 && command[1].equals("set")) {
-					try {
-						Viewport.globaltimer = Long.parseLong(command[2]);
-						output += "Time set to " + Viewport.globaltimer + "\n";
-					} catch (NumberFormatException e) {
-						output += "\"" + command[2]
-								+ "\" is not a valid time. Use \"!time ?\" for help\n";
-					}
-				}
-			}
-			break;
-
-		case "!save":
-			saver.save(world, world.regionGenerator);
-			output = null;
-			break;
-
-		case "!load":
-			saver.load(world);
-			output = null;
-			break;
-
-		// "!fly" command, changes flying state
-		case "!f":
-		case "!fly":
-			character.flying = !character.flying;
-			output = null;
-			break;
-		case "!addhealth":
-			character.doHit(-10000);
-			break;
-
-		case "!explode":
-			Point p = new Point((int) character.getHitbox().getX(),
-					(int) character.getHitbox().getY());
-			world.explode(p, 20);
-			output = null;
-			break;
-		}
-		return output;
 	}
 
 	private String runNetworkCommand(String[] command) {
